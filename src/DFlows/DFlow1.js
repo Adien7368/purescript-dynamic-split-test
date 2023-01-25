3511c7a71ee18eadfbec8258925f98c51e115071
Avinash Verma <avinashverma@Avinashs-MacBook-Pro.local>
2023-01-25 15:41:48 +0530

    instruction to switch dynamic import to static import

--- src/DFlows/DFlow1.js
+++ src/DFlows/DFlow1.js
@@ -1,6 +1,11 @@
+// import * as Flow1 from './../Flows.Flow1/index.js';
 
 export function dflow (onError, onSuccess) {
+    // Comment below line to disable dynamic import
+    // and Uncomment import and 7 line to create a single bundle
     import ('./../Flows.Flow1/index.js').then(module => onSuccess(module.flow)).catch(e => console.error(e));
+    // onSuccess(Flow1.flow) 
+
     return function (cancelError, onCancelerError, onCancelerSuccess) {
       onCancelerSuccess();
     };
